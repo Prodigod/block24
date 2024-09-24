1c4cfb053b6381cbf844664cc1569ec80677276e
Josh <flinkeydink@gmail.com>
2024-09-24 04:47:31 -0400

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import {puppyList} from './data.js'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { puppyList } from './data.js';
+import './index.css'
 
 function App() {
   const [count, setCount] = useState(0);
   const [puppies, setPuppies] = useState(puppyList);
-  console.log(puppies)
+  const [featPupId, setFeatPupId] = useState(null);
+
+  const handleClick = (id) => {
+    setFeatPupId(id); // Set the clicked puppy ID as the featured puppy
+  };
+
+  
+  const featuredPup = puppies.find((puppy) => puppy.id === featPupId);
+
   return (
     <>
       <div>
-        {
-          puppies.map((puppy) => {
-            return <p key={puppy.id}>{puppy.name}</p>;
-          })
-        }
+        {puppies.map((puppy) => (
+          <p onClick={() => handleClick(puppy.id)} key={puppy.id}>
+            {puppy.name}
+          </p>
+        ))}
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -23,6 +32,18 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+
+      
+      {featuredPup && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -36,7 +57,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
